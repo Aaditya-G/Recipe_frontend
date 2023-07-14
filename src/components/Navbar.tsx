fddae036a87be697fc2bd2b4b4cbebe10d4103a1
yash <yp969803@gmail.com>
2023-07-14 13:01:57 +0530

    navbar by sudo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,99 @@
+'use client';
+import React from 'react'
+import {useState,useEffect} from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+// Token 8aa2bb9efacee868d83ac03960e12f164d300e51
+import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
+const Navbar = () => {
+	const [loginStatus,setLoginStatus]=useState(false)
+	const router = useRouter()
+    
+	const pathname = usePathname()
+	
+	const getLoginStatus=async()=>{
+		try{
+		let authtoken =localStorage.getItem('auth-token')
+		let response;
+		if(authtoken){
+			 response = await fetch('http://localhost:8000/auth/test_token', {
+			method: "GET", // *GET, POST, PUT, DELETE, etc.
+			mode:'cors',
+			headers: {
+			  "accept": "application/json",
+			  "Authorization":`Token ${authtoken}`
+			}, 
+		  });
+		}
+		else{
+		    response = await fetch('http://localhost:8000/auth/test_token', {
+			method: "GET", // *GET, POST, PUT, DELETE, etc.
+			mode:'cors',
+			headers: {
+			  "accept": "application/json",
+			  "Authorization":`Token ${authtoken}`
+			}, 
+		  });
+		}
+		  console.log(response.status)
+		  if(response.status==200){
+			setLoginStatus(true)
+			
+		  }
+		  else{
+			setLoginStatus(false)
+		  }
+		}
+		catch(e){
+			console.log(e)
+		}
+		  
+	}
+	useEffect(()=>{
+		getLoginStatus()
+	},[loginStatus])
+
+  return (
+    <>
+	
+    <nav className="bg-gradient-to-r from-pink-200 to-white w-100 px-8 md:px-auto">
+	<div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
+	<img src="../public/navlogo.png" alt="Logo" />
+		<div className="text-pink-600 md:order-1">
+	
+		</div>
+		<div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
+			<ul className="flex font-semibold justify-between">
+                
+				<li className="md:px-4 md:py-2 text-black "><a href="/home">Home</a></li>
+				<li className="md:px-4 md:py-2 hover:text-black"><a href="/profile">Profile</a></li>
+				<li className="md:px-4 md:py-2 hover:text-black"><a href="/posts">MyCookBook</a></li>
+				<li className="md:px-4 md:py-2 hover:text-black"><a href="/about">About Us</a></li>
+				
+			</ul>
+		</div>
+		<div className="order-2 md:order-3 ">
+			<button className="px-4 py-2 bg-blue-950 hover:bg-blue-800 text-white rounded-xl flex items-center gap-2">
+               
+                {
+					loginStatus? (<Link href='#' onClick={()=>{
+						try{
+							localStorage.removeItem('auth-token')
+							getLoginStatus()
+							router.push('/')
+						}
+						catch(e){
+							console.log(e)
+						}
+					}}>LogOut</Link>):(<Link href='/login'>LogIn</Link>)
+				}
+            </button>
+		</div>
+	</div>
+</nav>
+</>
+  )
+}
+
+export default Navbar
