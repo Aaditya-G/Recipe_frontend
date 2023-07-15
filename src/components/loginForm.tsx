4db7ef5919b63df94c58ee191e78be5d8e52b986
Aaditya <aadityagupta400@gmail.com>
2023-07-15 11:33:30 +0530

    adds the feature to add a post to liked

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,72 +1,62 @@
-'use-client'
+"use-client";
 import React from "react";
-import axios from 'axios';
-
-
+import axios from "axios";
 
 const LoginForm = () => {
+  // const router = useRouter ();
 
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-      // const router = useRouter ();
+  const login = async () => {
+    try {
+      const response = await axios.post("http://localhost:8000/auth/login", {
+        username,
+        password,
+      });
 
-    const [username, setUsername] = React.useState("");
-    const [password, setPassword] = React.useState("");
+      const token = response.data.token;
 
+      sessionStorage.setItem("auth-token", token);
 
-    const login = async () => {
-        try {
-            const response = await axios.post('http://localhost:8000/auth/login' , {
-                username,
-                password
-            });
-
-            const token = response.data.token;
-            
-            sessionStorage.setItem('auth-token' , token)
-
-          
-            // router.push('/home')
-            window.location.href = "http://localhost:3000/home"
-        } catch (error) {
-            console.error(error);
-            throw new Error('Login failed');
-        }  
-       
-        }
-
-
+      // router.push('/home')
+      window.location.reload()
+    } catch (error) {
+      console.error(error);
+      throw new Error("Login failed");
+    }
+  };
 
   return (
     <div>
-      <form  onSubmit={(e) => {
+      <form
+        onSubmit={(e) => {
           e.preventDefault();
           login();
         }}
       >
-        <input 
-        type="text" 
-        name="username" 
-        value={username} 
-        placeholder="username"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-        onChange={(e) => setUsername(e.target.value)}
+        <input
+          type="text"
+          name="username"
+          value={username}
+          placeholder="username"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          onChange={(e) => setUsername(e.target.value)}
         />
         <br />
-        <input 
-        type="text" 
-        name="password" 
-        placeholder="password" 
-        value={password}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-      
-        onChange={(e) => setPassword(e.target.value)}
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          value={password}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          onChange={(e) => setPassword(e.target.value)}
         />
         <br />
         <button type="submit">Login</button>
       </form>
     </div>
-    
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
